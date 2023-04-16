d24dc93b02ef47eddee255c74192b382d996f038
Marlou Pepito <marloupepito07@gmail.com>
2023-04-16 20:52:58 +0800

    dapoidaoiwdpoaiwdpoawipdowa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,8 +11,8 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-      <script src="{{ asset('js/app.js') }}" defer></script> 
-    
+     <script src="{{ asset('js/app.js') }}" defer></script>
+      
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
         <!-- Styles -->
         <!-- <link rel="manifest" href="manifest.json" /> -->
--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/user', function (Request $request) {
-    return '$request->user()';
+    return $request->user();
 });
 
 Route::middleware('auth:sanctum')->get('/authenticated', function () {
